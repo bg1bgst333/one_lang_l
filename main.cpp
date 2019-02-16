9b365d34599d09de8e4e8296e9550522236c66f8
Hiroki Miura <h.miura@localhost.localdomain>
2019-02-16 20:47:28 +0900

    -printfで"Hello, OneLang!"の表示.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダ
+#include <stdio.h> // 標準入出力
+
+// main関数の定義
+int main(){
+
+  // "Hello, OneLang!"と出力.
+  printf("Hello, OneLang!\n"); // "Hello, OneLang!"と出力.
+
+  // プログラムの終了
+  return 0; // returnで0を返して正常終了.
+
+}
